ac89b67060fd0c857c128247a8da4b15dfe48928
Dmalex166 <152544901+Dmalex166@users.noreply.github.com>
2023-12-07 17:52:16 -0500

    Finished spaceinvaders (I think)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,17 @@ class GameScene extends Phaser.Scene {
   constructor() {
     super();
     console.log("constructor");
+    this.player;
+    this.cursors;
+    this.projectileImg;
+    this.projectileState = "ready";
+    this.enemies;
+    this.enemySpeed = 150;
+    this.numEnemies = 6;
+    this.scoretext;
+    this.score = 0;
+    this.gameOverText;
+    this.gameOver = false;
   }
   preload() {
     console.log("preload START");
@@ -26,9 +37,110 @@ class GameScene extends Phaser.Scene {
   }
   create() {
     console.log("create");
+    this.add.image(480, 360, "background");
+    this.player = this.physics.add.sprite(480, 600, "player");
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.player.setCollideWorldBounds(true);
+    this.projectileImg = this.physics.add.sprite(-1440, -1920, "projectile");
+    this.projectileImg.visible = false;
+    this.enemies = this.physics.add.group();
+    this.setEnemies();
+    this.physics.add.collider(
+      this.player,
+      this.enemies,
+      this.onPlayerHitEnemy,
+      null,
+      this
+    );
+    this.physics.add.overlap(
+      this.projectileImg,
+      this.enemies,
+      this.onProjectileHitEnemy,
+      null,
+      this
+    );
+    this.scoreText = this.add.text(16, 16, "Score: 0", {
+      fontSize: "32px",
+      fill: "#000",
+    });
+    this.gameOverText = this.add.text(config.width / 2, 400, "Game Over", {
+      fontSize: "64px",
+      fill: "#000",
+    });
+    this.gameOverText.visible = false;
   }
   update() {
     console.log("update");
+    if (this.cursors.left.isDown) {
+      this.player.x -= 10;
+    }
+    if (this.cursors.right.isDown) {
+      this.player.x += 10;
+    }
+    if (this.cursors.space.isDown) {
+      if (this.projectileState == "ready") {
+        this.fireProjectile();
+      }
+    }
+    if (this.projectileImg.y <= 16) {
+      this.resetProjectile();
+    }
+    this.enemies.children.iterate((child) => {
+      const body = child.body;
+      if (body.x < 0) {
+        body.setVelocityX(this.enemySpeed);
+        body.y += 64;
+      } else if (body.x > 896) {
+        body.setVelocityX(this.enemySpeed * -1);
+        body.y += 64;
+      }
+    });
+  }
+  fireProjectile() {
+    this.projectileState = "fire";
+    this.projectileImg.visible = true;
+    this.projectileImg.body.enable = true;
+    this.projectileImg.x = this.player.x;
+    this.projectileImg.y = this.player.y;
+    this.projectileImg.setVelocityY(-250);
+  }
+  resetProjectile() {
+    if (this.projectileState === "ready") {
+      return;
+    }
+    this.projectileState = "ready";
+    this.projectileImg.setVelocityY(0);
+    this.projectileImg.visible = false;
+  }
+  setEnemies() {
+    for (let i = 0; i < this.numEnemies; i++) {
+      this.enemies.create(
+        Phaser.Math.Between(64, 896),
+        Phaser.Math.Between(64, 296),
+        "enemy"
+      );
+    }
+    this.enemies.setVelocityX(this.enemySpeed * -1);
+  }
+  onProjectileHitEnemy(projectileImg, enemy) {
+    enemy.disableBody(true, true);
+    projectileImg.body.enable = false;
+    this.resetProjectile();
+    this.score += 1;
+    this.scoreText.setText(`Score: ${this.score}`);
+  }
+  onPlayerHitEnemy(player) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    this.gameOver = true;
+    this.showGameOverText();
+  }
+  showGameOverText() {
+    this.gameOverText.setOrigin(0.5);
+    this.gameOverText.visible = true;
+    this.enemies.children.iterate((child) => {
+      child.y = config.height * 2;
+    });
   }
 }
 const config = {
